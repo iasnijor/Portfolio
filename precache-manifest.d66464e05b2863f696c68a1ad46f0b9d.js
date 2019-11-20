d4ce95c0fa1451bfbc78354dee80b39195a6c562
Rojen Shahi <32856257+iasnijor@users.noreply.github.com>
2019-11-19 21:09:33 -0500

    Updates

---
+++ precache-manifest.d66464e05b2863f696c68a1ad46f0b9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c0979cde8435c44431cd679c6d1d43",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f9c20bde3ab18ecc6b66",
+    "url": "/portfolio/static/css/main.1eefb7ed.chunk.css"
+  },
+  {
+    "revision": "692c9e7965f2a1c84f3c",
+    "url": "/portfolio/static/js/2.a62e0b28.chunk.js"
+  },
+  {
+    "revision": "f9c20bde3ab18ecc6b66",
+    "url": "/portfolio/static/js/main.3067b261.chunk.js"
+  },
+  {
+    "revision": "85a750aecfe4e9050b45",
+    "url": "/portfolio/static/js/runtime-main.97659024.js"
+  }
+]);
